babe42ca1f6f22f4faae28112a7fc7136f51170f
Vanessasaurus <814322+vsoch@users.noreply.github.com>
2022-11-15 21:22:28 -0700

    use same path filter as in shpc-registry

--- .github/scripts/update_biocontainers.py
+++ .github/scripts/update_biocontainers.py
@@ -135,11 +135,37 @@ def main():
     # Write skips back to file for faster parsing
     shpc.utils.write_json(sorted(list(skips)), skips_file)
 
-
+    
 def include_path(path):
     """
-    Filter out binaries that are in system bins.
+    Filter out binaries in system bins, and various manual filters
     """
+    for ending in [
+        "post-link.sh",
+        ".debug",
+        "pre-link.sh",
+        ".so",
+        ".dll",
+        ".gz",
+        ".dna",
+        ".dox",
+        ".db",
+        ".db3",
+        ".config",
+        ".defaults",
+        ".dat",
+        ".dn",
+        ".d",
+        ".md",
+    ]:
+        if path.endswith(ending):
+            return False
+    if os.path.basename(path).startswith("_"):
+        return False
+    if os.path.basename(path).startswith("."):
+        return False
+    if "[" in path or "]" in path or "README" in path:
+        return False
     return "sbin" not in path and "/usr/bin" not in path and not path.startswith("/bin")
 
 
